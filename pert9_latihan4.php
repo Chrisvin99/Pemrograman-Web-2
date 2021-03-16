28972d6a8697c170a548b6a69aec523aa2c1cb4a
Chrisvin99 <79969669+Chrisvin99@users.noreply.github.com>
2021-03-16 19:03:32 +0700

    Add files via upload

---
+++ pert9_latihan4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanggal</title>
+	<style type="text/css">
+		h3	{
+			color: blue;
+			background-color: green;
+		}
+	</style>
+</head>
+<body>
+	<font size="10px">
+		<h4>FUNGSI DATE</h4>
+		<h4>Cristian Calvin</h4>
+	<?php 
+	echo "Sekarang Tanggal";
+	echo date('d-F-Y');
+	echo "<br> dan jam";
+	echo date('h:i:s A');
+	 ?>
+	</font>
+</body>
+</html>
